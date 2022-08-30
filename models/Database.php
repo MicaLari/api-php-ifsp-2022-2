0b10366c34d7a24e2c69160931b2f1160d689e52
Micaella Larissa <micaella.r@aluno.ifsp.edu.br>
2022-08-30 17:50:04 -0300

    Create Database

---
+++ models/Database.php
@@ -0,0 +1,18 @@
+<?php
+
+class Database{
+    function connect(){
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "";
+
+        try {
+         $conn = new
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+}
+
+?>
