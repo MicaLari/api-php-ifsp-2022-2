e63e1c6e6a5708a5647fb9094188f22dd7d4d951
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-09-06 16:02:35 -0300

    Criação do Models_Foldes

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('BASE_PATH','/pj3/api-php-ifsp-2022-2/');
+define('BASE_PATH','/api-php-ifsp-2022-2/');
 define('CONTROLLERS_FOLDER','controllers/');
 define('HELPERS_FOLDER','helpers/');
-
+define('MODELS_FOLDER','models/');
 ?>
