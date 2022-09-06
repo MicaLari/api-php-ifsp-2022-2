8c57f7ee1ebfbe101cdab694f1d591e21549080a
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-09-06 16:13:28 -0300

    Acresimo do Models_folder

--- helpers/autoloaders.php
+++ helpers/autoloaders.php
@@ -7,6 +7,9 @@ function myLoaders($class_name){
     if(file_exists(HELPERS_FOLDER . $class_name . '.php')){
         require HELPERS_FOLDER . $class_name . '.php';
     }
+    if(file_exists(MODELS_FOLDER . $class_name . '.php')){
+        require MODELS_FOLDER . $class_name . '.php';
+    }
 }
 //Automatoriza o carregamento das classes
 spl_autoload_register('myLoaders');
