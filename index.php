0c6d87d13ab6ab244c5f30942da0a1de852e6fe7
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-09-12 14:57:05 -0300

    Arquivo com a importação da configuração

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
+//importa o arquivo de configuracao
+require 'config.php';
+require HELPERS_FOLDER.'autoloaders.php';
 
-require('router.php');
+$route = new Router();
+$route->gateKeeper();
 
+$output = new Output();
+$output->notFound();
 ?>
