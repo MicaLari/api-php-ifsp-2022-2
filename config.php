926d320877d60e70ebc0e89be56ffad1ce411ad6
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-09-26 17:47:16 -0300

    add de  DATABASE

--- config.php
+++ config.php
@@ -1,13 +1,17 @@
 <?php
-
+// PATHS and FOLDERS
 define('BASE_PATH','/api-php-ifsp-2022-2/');
 define('CONTROLLERS_FOLDER','controllers/');
 define('HELPERS_FOLDER','helpers/');
 define('MODELS_FOLDER','models/');
 
-// CORS allowed hosts
-//public api
-//define('ALLOWED_HOSTS', '*')
-//private
-define('ALLOWED_HOSTS', 'http://localhost:3000')
+//DATABASE
+define('DB_NAME','film_pj');
+define('DB_USER','root');
+define('DB_HOST','localhost');
+define('DB_PASS','');
+
+// CORS
+define('ALLOWED_HOSTS', 'http://localhost:3000');
+define('ALLOWED_METHODS', 'GET, POST, OPTIONS, DELETE, PUT')
 ?>
