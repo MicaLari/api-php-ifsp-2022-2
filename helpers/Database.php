e0203a84f6f05453a836104568c1b1ae3db3c7ef
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-09-27 14:42:12 -0300

    add database constants to config

--- helpers/Database.php
+++ helpers/Database.php
@@ -1,18 +1,13 @@
 <?php 
 class Database{
     static function connect(){
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "film_pj";
         
         try {
-          $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-          // set the PDO error mode to exception
+            $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);          // set the PDO error mode to exception
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           return $conn;
         } catch(PDOException $e) {
-            Output::response($result, 500);
+            self::dbError($e);
         }
     }
 
